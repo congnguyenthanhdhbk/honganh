f9b905f1752bf8c349cdc96279980f888dcd1fe9
Cong Nguyen Thanh <57513462+congnguyenthanhdhbk@users.noreply.github.com>
2021-06-22 11:44:27 +0700

    first version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/congnguyenthanhdhbk/honganh
+
+go 1.16
---
+++ pkg/math/Add.go
@@ -0,0 +1,11 @@
+package math
+
+import "fmt"
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Logs(content string) {
+	fmt.Println(content)
+}
